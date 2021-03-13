4003a759e085daddd34ed31f27faac8104c14aec
DukhDmutro <dukhdmytro@gmail.com>
2021-03-13 17:43:15 +0200

    Chapter 3. Templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
 # app - instance of Flask class
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # app - package
 # The bottom import is a workaround to circular imports
@@ -9,4 +11,3 @@ app = Flask(__name__)
 # so putting one of the reciprocal imports at the bottom avoids the error
 # that results from the mutual references between these two files
 from app import routes
-
--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,10 @@
 @app.route decorator creates an association between
 the URL given as an argument and the function.
 """
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+
+from .fomrs import LoginForm
 
 
 @app.route("/")
@@ -21,3 +23,12 @@ def index():
         }
     ]
     return render_template("index.html", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('logined')
+        return redirect(url_for("index"))
+    return render_template("login.html", form=form, title="Sign In")
